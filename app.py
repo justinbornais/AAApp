624df4731107980e7889d9e91dce673c5b634515
Justin135 <justin.bornais@gmail.com>
2021-05-21 14:31:54 -0400

    Added relapse button.

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
-
-sober = False
-days = 0
+from kivy.lang import Builder
+from kivy.core.window import Window
 
 class MainApp(App):
-    
+    Window.clearcolor = (1, 1, 1, 1)
     days = 0
     
     def build(self):
@@ -16,13 +15,22 @@ class MainApp(App):
         
         label = Label(text='Welcome to Sobriety!',
                       size_hint=(0.8, 0.8),
-                      pos=(100, 100))
+                      pos=(100, 100),
+                      color=("000000"))
         
         layout.add_widget(label)
         
         soberButton = Button(text="Press to join sobriety!",
                         size_hint=(0.3, 0.3),
-                        pos_hint={"center_x": 0.5, "y": 0})
+                        pos_hint={"center_x": 0.2, "y": 0},
+                        color=("000000"),
+                        background_color=("#ffeec2"))
+        
+        relapseButton = Button(text="Press if you relapse",
+                        size_hint=(0.3, 0.3),
+                        pos_hint={"center_x": 0.8, "y": 0},
+                        color=("000000"),
+                        background_color=("#ffeec2"))
         
         def soberButtonFunc(soberButton):
             if soberButton.text == "Press to join sobriety!":
@@ -33,8 +41,16 @@ class MainApp(App):
                 self.days += 1
                 label.text = f"Welcome to Sobriety!\nYou are {self.days} days sober!"
         
+        def relapseButtonFunc(relapseButton):
+            self.days = 0
+            label.text = "So sorry to see you relapse!"
+            soberButton.text = "Press to join sobriety!"
+        
         soberButton.bind(on_press=soberButtonFunc)
         layout.add_widget(soberButton)
+        
+        relapseButton.bind(on_press=relapseButtonFunc)
+        layout.add_widget(relapseButton)
         
         return layout
 
